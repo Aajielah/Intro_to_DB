c11397d27c01211c8158f41a3d4c7f5ca312210d
Abdullahi Akanji Tjani <tijaniabdullahi2021@gmail.com>
2026-02-23 23:38:23 +0000

    sql mismatch

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,43 +1,43 @@
-CREATE DATABASE alx_book_store;
-
-CREATE TABLE Books(
-	book_id INT PRIMARY KEY,
-	title VARCHAR(130),
-	author_id INT,
-	price FLOAT,
-	publication_date DATE,
-	CONSTRAINT FK_author_id FOREIGN KEY (author_id)
-	REFERENCES Authors(author_id)
-);
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
 
 CREATE TABLE Authors(
-	author_id INT PRIMARY KEY,
-	author_name VARCHAR(215)
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
 
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id)
+        REFERENCES Authors(author_id)
+);
 
 CREATE TABLE Customers(
-	customer_id INT PRIMARY KEY,
-	customer_name VARCHAR(215),
-	email VARCHAR(215),
-	address TEXT
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
 );
 
 CREATE TABLE Orders(
-	order_id INT PRIMARY KEY,
-	customer_id INT,
-	order_date DATE,
-	CONSTRAINT FK_customer_id FOREIGN KEY (customer_id)
-	REFERENCES Customers(customer_id)
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id)
+        REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
-	orderdetailid INT PRIMARY KEY,
-	order_id INT,
-	book_id INT,
-	quantity FLOAT,
-	CONSTRAINT FK_order_id FOREIGN KEY (order_id)
-	REFERENCES Orders(order_id),
-	CONSTRAINT FK_book_id FOREIGN KEY (book_id)
-	REFERENCES Books(book_id)
-);git 
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id)
+        REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id)
+        REFERENCES Books(book_id)
+);
